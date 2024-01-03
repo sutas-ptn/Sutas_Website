4c5ae6dd6530838b5e8ae6c0becf8080e7f9d029
Sutas Pattanaworapongkul <sutas218@gmail.com>
2024-01-03 17:26:32 +0700

    third

--- portfolio/src/component/Hero.js
+++ portfolio/src/component/Hero.js
@@ -14,8 +14,10 @@ var prevScrollpos = window.scrollY;
       var currentScrollPos = window.scrollY;
         if (prevScrollpos > currentScrollPos) {
           document.getElementById("navbar").style.top = "0";
+          
         } else {
-          document.getElementById("navbar").style.top = "-50px";
+          document.getElementById("navbar").style.top = "-100px";
+          
         }
         prevScrollpos = currentScrollPos;
       }
@@ -30,7 +32,7 @@ export default function Hero() {
   return (
     <div className="min-h-screen bg-black text-white">
       <header className="absolute inset-x-0 top-0 z-50 ">
-        <div className='w-full fixed ' id='navbar'>
+        <div className='bg-black navc lg:bg-inherit lg:shadow-none w-full fixed   ' id='navbar'>
           <nav className="flex items-center justify-between p-6 lg:px-8 " aria-label="Global">
             <div className="flex lg:flex-1 ">
               <a href="#Home" className="text-white colorglow -m-1.5 p-1.5  ">
@@ -63,7 +65,7 @@ export default function Hero() {
           <div className="fixed inset-0 z-50 " />
           <Dialog.Panel className=" fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 ">
             <div className="flex items-center justify-between  ">
-              <a href="#Home" className="-m-1.5 p-1.5 colorglow ">
+              <a href="#Home"onClick={() => setMobileMenuOpen(false)} className="-m-1.5 p-1.5 colorglow ">
                 <span className="sr-only">Home</span>
                 <div className='text-2xl  font-normal '><span className=' font-normal'>S</span>utas</div>
               </a>
